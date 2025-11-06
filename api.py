71dcd48304495c5c1c184a6f5e3f7810b48617ec
Nithin Nathani <nithinchowdary1919@gmail.com>
2025-11-06 20:40:49 +0530

    Reformat api.py for consistency

--- api.py
+++ api.py
@@ -1,23 +1,17 @@
-# api.py
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model_handler import predict_sms
 
-
 app = FastAPI(title="SMS Spam Detection API")
-
 
 class Message(BaseModel):
     text: str
 
-
 @app.get("/")
 def root():
-return {"message": "SMS Spam Detection API. POST /predict with {text}."}
-
+    return {"message": "SMS Spam Detection API. POST /predict with {text}."}
 
 @app.post("/predict")
 def predict(message: Message):
-res = predict_sms(message.text)
-return res
-
+    res = predict_sms(message.text)
+    return res
